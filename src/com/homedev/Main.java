c73560d96affd88894ca7ccddfecf5132b976804
Alexandr Zheleznyakov <delta.agent00@gmail.com>
2020-04-30 09:43:23 +0300

    Lesson 5

--- src/com/homedev/Main.java
+++ src/com/homedev/Main.java
@@ -4,7 +4,10 @@ import com.homedev.mvp.view.ICache;
 import com.homedev.cache.SimpleCacheImpl;
 import com.homedev.mvp.model.entity.Author;
 import com.homedev.mvp.model.entity.Book;
+import com.homedev.mvp.view.IObserverSavedBook;
 import com.homedev.mvp.view.IStorage;
+import com.homedev.mvp.view.ISubscribeSavedBookListener;
+import com.homedev.observable.ObservableSavedBookImpl;
 import com.homedev.storage.MapStorageImpl;
 
 public class Main {
@@ -41,11 +44,18 @@ public class Main {
         System.out.println(book);
 
         final ICache cache = new SimpleCacheImpl();
-        final IStorage storage = new MapStorageImpl(cache);
+        final IObserverSavedBook observableSavedBook = new ObservableSavedBookImpl();
+        final ISubscribeSavedBookListener iSubscribeSavedBookListener = book1 ->
+                System.out.println("subscribe call: " + book1);
+        final IStorage storage = new MapStorageImpl(cache, observableSavedBook);
+
+        observableSavedBook.addSubscribe(iSubscribeSavedBookListener);
 
         storage.saveOrUpdateBook(book);
         final Book savedBook = storage.getBookById(book.getId());
 
         System.out.println(savedBook);
+
+        observableSavedBook.removeSubscribe(iSubscribeSavedBookListener);
     }
 }
--- src/com/homedev/storage/MapStorageImpl.java
+++ src/com/homedev/storage/MapStorageImpl.java
@@ -2,6 +2,7 @@ package com.homedev.storage;
 
 import com.homedev.mvp.view.ICache;
 import com.homedev.mvp.model.entity.Book;
+import com.homedev.mvp.view.IObserverSavedBook;
 import com.homedev.mvp.view.IStorage;
 
 import java.util.HashMap;
@@ -10,9 +11,11 @@ import java.util.Map;
 public class MapStorageImpl implements IStorage {
     private final ICache cache;
     private final Map<Integer, Book> db = new HashMap<>();
+    private final IObserverSavedBook observableBook;
 
-    public MapStorageImpl(ICache cache) {
+    public MapStorageImpl(ICache cache, IObserverSavedBook observableBook) {
         this.cache = cache;
+        this.observableBook = observableBook;
     }
 
     @Override
@@ -27,7 +30,13 @@ public class MapStorageImpl implements IStorage {
 
     @Override
     public void saveOrUpdateBook(Book book) {
+        final Book dbBook = getBookById(book.getId());
+
         db.put(book.getId(), book);
         cache.put(book);
+
+        if (dbBook == null) {
+            observableBook.onBookSaved(book);
+        }
     }
 }
